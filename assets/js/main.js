998717e0ca7b40a29ce32f61e5b84671912f6904
Tran Van Tien Phat <tvtp0503vn@gmail.com>
2024-12-25 18:54:29 +0700

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  var placeholderText = [
+    "Bạn muốn tìm gì?",
+    "Trang điểm",
+    "Dưỡng thể",
+    "Chăm sóc cá nhân",
+    "Chăm sóc cơ thể",
+  ];
+  $(".search-auto").placeholderTypewriter({ text: placeholderText });
+});
